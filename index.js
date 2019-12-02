b6322da6be45ef38c94f158f27e095f0646c7438
Nelly <nkhmeleva@yahoo.com>
2019-12-01 22:57:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(i => i*(-1))
+}
+function mapToNoChange(sourceArray){
+    return sourceArray.map( i => i)
+}
+function mapToDouble(sourceArray) {
+    return sourceArray.map(i => i*2)
+}
+function mapToSquare(sourceArray) {
+    return sourceArray.map(i => i**2)
+}
+function reduceToTotal(sourceArray, startingPoint=0){
+    return sourceArray.reduce((a,b) => a+b, startingPoint)
+}
+
+//finish reduce()
